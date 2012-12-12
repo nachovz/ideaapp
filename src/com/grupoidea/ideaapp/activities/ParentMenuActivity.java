3ab64b0dedf67ccec299c667e2a916296c042c61
rramos <rramos@stcsolutions.com.ve>
2012-12-12 01:42:03 -0430

    Maquetación Dashboard
    Implementación del RelativeLayout RowCliente, ajuste de elementos
    graficos en el DashboardActivity, agregados archivos no incluidos en la
    revision anterior.

--- src/com/grupoidea/ideaapp/activities/ParentMenuActivity.java
+++ src/com/grupoidea/ideaapp/activities/ParentMenuActivity.java
@@ -3,10 +3,12 @@ package com.grupoidea.ideaapp.activities;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.grupoidea.ideaapp.R;
 
 public abstract class ParentMenuActivity extends ParentActivity {
+	private TextView menuTituloTextView; 
 	
 	public ParentMenuActivity(boolean autoLoad, boolean hasCache) {
 		super(autoLoad, hasCache);
@@ -16,6 +18,8 @@ public abstract class ParentMenuActivity extends ParentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		super.setContentView(R.layout.parent_menu_layout);
+		
+		menuTituloTextView = (TextView) findViewById(R.id.menu_titulo_text_view);
 	}
 
 	@Override
@@ -28,4 +32,11 @@ public abstract class ParentMenuActivity extends ParentActivity {
 		parentInflater.addView(inflateView);
 	}
 	
+	/** Permite mostrar una cadena de texto en el centro del menú.
+	 *  @param titulo Cadena de texto que representa informacion relevante al Activity.*/
+	protected void setMenuTittle(String titulo) {
+		if(menuTituloTextView != null && titulo != null) {
+			menuTituloTextView.setText(titulo);
+		}
+	}
 }
